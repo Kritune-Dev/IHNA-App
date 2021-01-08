ea7ef4b8ecb37795cfd3ed75af663d496966a177
Corentin Bacconnais <corentin.bacconnais@gmail.com>
2021-01-08 20:59:59 +0100

    Fix route

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const ihnaAppRoutes = require('./routes/ihna_app.routes')
 
 app.use(express.json())
 
-app.use('/api/calendarservice', ihnaAppRoutes)
+app.use('/api/App', ihnaAppRoutes)
 
 app.use((error, req, res) => {
     res.status(500).json({message: error.message})
